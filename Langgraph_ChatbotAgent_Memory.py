252832db3c44e7793dbf864378b6ebd5b102a22e
Jerome Mohanan <2962518+jero2rome@users.noreply.github.com>
2024-07-07 20:34:34 +0200

    Add agent with memory

---
+++ Langgraph_ChatbotAgent_Memory.py
@@ -0,0 +1,83 @@
+import os
+import json
+from dotenv import load_dotenv
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+from langchain_core.messages import HumanMessage, ToolMessage
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langchain_core.tools import tool
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from typing import Annotated, Literal
+from typing_extensions import TypedDict
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+_ = load_dotenv()
+
+tavilyApiKey = os.getenv("TAVILY_API_KEY")
+openAIApiKey = os.getenv("OPENAI_API_KEY")
+anthropicApiKey = os.getenv("ANTHROPIC_API_KEY")
+
+# Enable tracing if required
+langsmithApiKey = os.getenv("LANGSMITH_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "LangGraph Tutorial"
+
+# llm = ChatAnthropic(model="claude-3-haiku-20240307")
+
+llm = ChatOpenAI(model="gpt-4o")
+
+memory = SqliteSaver.from_conn_string(":memory:")
+
+tool = TavilySearchResults(max_results=2)
+tools = [tool]
+
+llm_with_tools = llm.bind_tools(tools)
+
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+
+# The `tools_condition` function returns "tools" if the chatbot asks to use a tool, and "__end__" if
+# it is fine directly responding. This conditional routing defines the main agent loop.
+graph_builder.add_conditional_edges("chatbot", tools_condition)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.set_entry_point("chatbot")
+graph = graph_builder.compile(checkpointer=memory)
+
+config = {"configurable": {"thread_id": "1"}}
+
+while True:
+    user_input = input("User: ")
+    if user_input.lower() in ["quit", "exit", "q"]:
+        print("Goodbye!")
+        break
+
+    # The config is the **second positional argument** to stream() or invoke()!
+    events = graph.stream(
+        {"messages": [("user", user_input)]}, config, stream_mode="values"
+    )
+
+    for event in events:
+        event["messages"][-1].pretty_print()
